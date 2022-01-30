bb7ff51db541ea7548b682c5e215b6361625831b
Jo-009 <94837678+Jo-009@users.noreply.github.com>
2022-01-30 13:57:35 +0800

    Add files via upload

---
+++ mynews2-WEB/src/main/java/com/news/app/model/NewsSet.java
@@ -0,0 +1,15 @@
+package com.news.app.model;
+
+import java.util.ArrayList;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class NewsSet {
+	private String status;
+	private Integer totalResults;
+	private ArrayList<Articles> articles;
+
+}
---
+++ mynews2-WEB/src/main/java/com/news/app/repo/NewsSetRepo.java
@@ -0,0 +1,9 @@
+//package com.news.app.repo;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.news.app.model.NewsSet;
+//
+//public interface NewsSetRepo extends JpaRepository<NewsSet, Integer> {
+//
+//}
